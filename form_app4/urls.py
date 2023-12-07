e300b5920b3ab9c41908098614081cf9d639f0fd
jgrynczewski <jgrynczewski@gmail.com>
2023-12-07 18:04:21 +0100

    CRUD - C, R list, R detail

--- form_app4/urls.py
+++ form_app4/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from form_app4.views import task_create_view, task_list_view
+from form_app4.views import task_create_view, task_list_view, task_detail_view
 
 app_name = 'form_app4'
 
 urlpatterns = [
+    # C z CRUD
     path('tasks/create/', task_create_view, name='create_task'),
+    # R z CRUD (lista)
     path('tasks/', task_list_view, name='task_list'),
+
+    # R z CRUD (szczegół)
+    path('tasks/<int:task_id>/', task_detail_view, name='task_detail')
 ]
--- form_app4/views.py
+++ form_app4/views.py
@@ -1,9 +1,10 @@
 # formularz - metoda POST (redirect)
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, Http404
 
 TASKS = []
 
 
+# C z CRUD
 def task_create_view(request):
     if request.method == "GET":
         return render(
@@ -19,11 +20,28 @@ def task_create_view(request):
         return redirect('form_app4:task_list')
 
 
+# R (lista) z CRUD
 def task_list_view(request):
     return render(
         request,
         'form_app4/task_list.html',
         {
             'tasks': TASKS
+        }
+    )
+
+
+# R (szczegół) z CRUD
+def task_detail_view(request, task_id):
+    if task_id > len(TASKS):
+        raise Http404()
+
+    task = TASKS[task_id-1]
+    return render(
+        request,
+        'form_app4/task_detail.html',
+        {
+            'task_id': task_id,
+            'task': task
         }
     )
